96d2deffa7bf3eb714c65c385d1ebb346fccb533
rizaru22 <rizaru.2.2@gmail.com>
2025-03-07 11:37:50 +0700

    ADD Template Admin

---
+++ resources/views/components/menu.blade.php
@@ -0,0 +1,8 @@
+
+
+<li class="sidebar-item  {{ $active ? 'active' : '' }}">
+    <a href="{{ $link }}" class='sidebar-link'>
+        <i class="{{ $icon }}"></i>
+        <span>{{ $name }}</span>
+    </a>
+</li>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,17 +6,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" crossorigin href="{{ asset('/assets/compiled/css/app.css') }}">
     <link rel="stylesheet" crossorigin href="{{ asset('/assets/compiled/css/app-dark.css') }}">
-    <link rel="stylesheet" crossorigin href="{{ asset('/assets/compiled/css/auth.css') }}">
+
     <link rel="stylesheet" href="{{ asset('assets/extensions/@fortawesome/fontawesome-free/css/all.min.css') }}">
-  
-    <title>{{ $title ?? config('app.name')}}</title>
+
+    <title>{{ $title ?? config('app.name') }}</title>
 </head>
 
 <body>
-    <div id="auth">
-    {{ $slot }}
+    <div id="app">
+        <div id="sidebar">
+            <x-side-bar/>
+        </div>
+        <div id="main">
+            <header class="mb-3">
+                <a href="#" class="burger-btn d-block d-xl-none">
+                    <i class="bi bi-justify fs-3"></i>
+                </a>
+            </header>
+
+            {{ $slot }}
+
+
+            <footer>
+                <div class="footer clearfix mb-0 text-muted">
+                    <div class="float-start">
+                        <p>2023 &copy; Mazer</p>
+                    </div>
+                    <div class="float-end">
+                        <p>Crafted with <span class="text-danger"><i class="bi bi-heart-fill icon-mid"></i></span>
+                            by <a href="https://saugi.me">Saugi</a></p>
+                    </div>
+                </div>
+            </footer>
+        </div>
     </div>
+
+
     <script src="{{ asset('assets/static/js/initTheme.js') }}"></script>
+    <script src="{{ asset('assets/static/js/components/dark.js') }}"></script>
+    <script src="{{ asset('assets/extensions/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
+
+
+    <script src="{{ asset('assets/compiled/js/app.js') }}"></script>
 </body>
 
 </html>
